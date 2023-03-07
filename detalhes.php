36251ed382be38035b77b3007a705a38beaf514b
Guilherme Santos <117474585+guisantosdesenvolvedor@users.noreply.github.com>
2023-03-07 01:42:33 -0300

    Update detalhes.php

--- detalhes.php
+++ detalhes.php
@@ -1,10 +1,24 @@
+<?php 
+    include 'conn/conecta.php';
+    $id = $_GET['id'];
+    $lista = $conn->query("select * from vw_produtos where id like '%$id%';");
+    $row =  $lista->fetch_assoc();
+    $num_linhas = $lista->num_rows;
+?>
+
 <!DOCTYPE html>
-<html lang="pt_BR">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/estilo.css">
+    <!-- Link do css e reset -->
+    <link rel="stylesheet" type="text/css" href="CSS/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+    <link rel="shortcut icon" href="./images/logo-shorticon.png" type="image/png">
+    <!-- Link do Angular -->
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+    <title>Detalhes</title>
 </head>
 <body>
 <nav id="navbar">
@@ -18,19 +32,42 @@
                 <li><a href="index.php#contato">CONTATO</a></li>
             </ul>
         </div>
-    </nav>
-<div>
-    <div class="coll">
-                <div>
-                    <img src="img/visao.jpg" alt="">
-                </div>
-                <div>
-                    <img src="img/valores.jpg" alt="">
-                </div>
-    </div>
-        <div class="lrowsl">
-                
+</nav>  
+  <div id="container-detalhes">
+        <!-- Top para voltar -->
+        <h2>
+            <a href="javascript:window.history.go(-1)" role="button" >
+                <span class="btn-voltar">
+                    <i class="fa-solid fa-circle-chevron-left"></i>
+                </span>
+            </a>&nbsp;
+            <?php echo $row['nome']; ?>
+        </h2>             
+        <div id="container-main">
+            <img src="img/<?php echo $row['imagem']; ?>" alt="">
+            <div>
+                <p>
+                    <h2><?php echo $row['descricao']; ?></h2> <br> <br>
+                    <?php echo $row['resumo']; ?> <br><br>  
+                </p>
+                <?php
+                    // Define o valor do desconto como 10%
+                        $desconto = 0.1;
+                    // Obtém o custo total do produto
+                        $custo_total = $row['custo_total'];
+                    // Calcula o valor com desconto
+                        $preco_com_desconto = $custo_total * (1 - $desconto);
+                ?>
+                <p id="preco">
+                    <h2>
+                        VALOR: R$
+                        <?php echo number_format($preco_com_desconto, 3, '.', ','); // formata para exibir com duas casas decimais e separador de milhar com ponto ?>
+                        <span class="preco-original">
+                            <strike>R$<?php echo number_format($custo_total, 3, '.', ','); ?></strike>
+                        </span>
+                    </h2>
+                </p>
+            </div>
         </div>
-</div>
 </body>
 </html>

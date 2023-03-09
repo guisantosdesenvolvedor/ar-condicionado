5ae198d96bffa376a90378fbb226707ebf5a4f9e
guisantosdesenvolvedor <guikerme.1707@gmail.com>
2023-03-09 16:21:01 -0300

    colocando serviços em responsivo

--- servicos.php
+++ servicos.php
@@ -9,87 +9,6 @@
     <link rel="stylesheet" media="screen and (max-width: 768px)" href="css/mobile.css">
 </head>
 <body>
-<!-- <div class="area_servicos" id="servicos">
-    <div class="paragrafo">
-        <h1>Serviços</h1>
-    </div>
-    <div class="img_linha"><div>
-    </div>
-       <div class="area-servico-rows">
-            <ul>
-                <li>
-                  <img class="" src="img/linha1.png" alt="" style="padding: 0px 40px 0px 0px;" width="350px" height="50px">
-                </li>
-            </ul>
-       </div>
-    </div>
-    <div class="area-servico1">
-        <div class="area-servico-rows">
-            <ul>
-                <li>
-                    <a href=""><img src="img/ar2.webp" alt=""></a>
-                </li>
-            </ul>
-        </div>
-    </div>
-    <div class="area-servico-rows">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci facere, unde asperiores, cupiditate libero saepe temporibus .</p>
-    </div>
-    <div class="area-servico-rows">
-        <ul>
-            <li>
-                <a href=""><img src="img/ar3.webp" alt=""></a>
-            </li>
-        </ul>
-    </div>
-    <div class="area-servico-rows">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci facere, unde asperiores, cupiditate libero saepe temporibus .</p>
-    </div>
-    <div class="area-servico-rows">
-        <ul>
-            <li>
-                <a href=""><img src="img/ar4.png" alt=""></a>
-            </li>
-        </ul>
-    </div>
-        <div class="area-servico-rows">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id laudantium ipsam adipisci odit sit quo quos libero sunt illo iusto provident blanditiis, facere, nostrum tempore officiis? Velit commodi sunt tenetur.</p>
-        </div>
-    <div class="area-servico1">
-        <div class="area-servico-rows">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id laudantium ipsam adipisci odit sit quo quos libero sunt illo iusto provident blanditiis, facere, nostrum tempore officiis? Velit commodi sunt tenetur.</p>
-        </div>
-    </div>
-    <div class="area-servico-rows">
-        <ul>
-            <li>
-                <a href=""><img src="img/ar5.webp" alt=""></a>
-            </li>
-        </ul>
-    </div>
-    <div class="area-servico-rows">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci facere, unde asperiores, cupiditate libero saepe temporibus .</p>
-    </div>
-    <div class="area-servico-rows">
-        <ul>
-            <li>
-                <a href=""><img src="img/ar6.webp" alt=""></a>
-            </li>
-        </ul>
-    </div>
-    <div class="area-servico-rows">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci facere, unde asperiores, cupiditate libero saepe temporibus .</p>
-    </div>
-    <div class="area-servico-rows">
-        <ul>
-            <li>
-                <a href=""><img src="img/ar4.png" alt=""></a>
-            </li>
-        </ul>
-    </div>
-
-</div> -->
-
 <nav class="navbar-servicos">
     <div class="cabecalho-servicos">
         <h1>SERVIÇOS</h1>
@@ -114,28 +33,25 @@
         <div class="txt-servicos">
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis illo magnam debitis!</p>
         </div>
-    </div>
-        <div class="trocadelinha-servicos">
-             <div class="txt-servicos">
+        <div class="txt-servicos">
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis ipsa possimus accusantium.</p>
-            </div>
-            <div class="img-servicos">
-                <img src="img/ar2.webp" alt="">
-            </div>
-            <div class="txt-servicos">
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt accusamus delectus rem.</p>
-            </div>
-            <div class="img-servicos">
-             <img src="img/ar3.webp" alt="">
-            </div>
-            <div class="txt-servicos">
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis illo magnam debitis!</p>
-            </div>
-            <div class="img-servicos">
-            <img src="img/ar4.png" alt="">
-            </div>
-            
         </div>
+        <div class="img-servicos">
+            <img src="img/ar2.webp" alt="">
+        </div>
+         <div class="txt-servicos">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt accusamus delectus rem.</p>
+        </div>
+        <div class="img-servicos">
+             <img src="img/ar3.webp" alt="">
+         </div>
+        <div class="txt-servicos">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis illo magnam debitis!</p>
+        </div>
+        <div class="img-servicos">
+            <img src="img/ar4.png" alt="">
+        </div>
+    </div>
 </nav>
 </body>
 </html>

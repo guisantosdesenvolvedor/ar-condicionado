e1378294c1b741c2ffc7aa4c4df719a86ffccaef
guiguivida183882 <guikerme.1707@gmail.com>
2023-03-10 17:10:01 -0300

    fazendo a pg.produtos e deixando responsiva

--- produtos.php
+++ produtos.php
@@ -12,134 +12,99 @@ include 'conn/conecta.php';
     <link rel="stylesheet" media="screen and (max-width: 768px)" href="css/mobile.css">
 </head>
 <body  ng-app="meuApp" ng-controller="meuController">
-<!-- <nav id="produtos">
-        <div >
-            <div class="alinha_meio">
-                <h1>Produtos</h1>
+<nav id="produtos">
+        <div class="cabecalho-produtos">
+            <h1>Produtos</h1>
+            <img src="img/linhapequena.png" alt="">
+        </div>
+            <div class="linha w50">
+                <div ng-mouseover="funcaoArcondicionado()">
+                    AR-CONDICIONADO
+                </div>
+                <div ng-mouseover="funcaoInverter()">
+                    AR-CONDICIONADO INVERTER
+                </div>
+                <div ng-mouseover="funcaoMultisplit()">
+                    MULTI SPLIT
+                </div>
+                <div ng-mouseover="funcaoEnergiasolar()">
+                    ENERGIA SOLAR
+                </div>
             </div>
-            <div class="img_abaixo_produtos">
-                <div class="alinha_meio">
-                    <ul>
-                        <li>
-                            <img src="img/linhapequena.png" alt="">
-                        </li>
-                    </ul>
+
+            <div class="imagens-linha-produtos w100" ng-show="arcondicionado">
+                <div class="imagem-produtos">
+                <a href="detalhes.php?id=1"><img src="img/arcondicionado1.webp" alt=""></a>
+                </div>
+                <div class="imagem-produtos">
+                <a href="detalhes.php?id=2"><img src="img/arcondicionado2.webp" alt=""></a>
+                </div>
+                <div class="imagem-produtos">
+                <a href="detalhes.php?id=3"><img src="img/arcondicionado3.webp" alt=""></a>
+                </div>
+                <div class="imagem-produtos"> 
+                <a href="detalhes.php?id=4"><img src="img/arcondicionado4.webp" alt=""></a>
+                </div>
+                <div class="imagem-produtos">
+                <a href="detalhes.php?id=5"><img src="img/arcondicionado5.webp" alt=""></a>
+                </div>
+            </div>
+
+            <div class="imagens-linha-produtos w100" ng-show="inverter">
+                <div class="imagem-produtos">
+                <a href="detalhes.php?id=6"><img src="img/arp1.webp" alt=""></a>
+                </div>
+                <div class="imagem-produtos">
+                <a href="detalhes.php?id=7"><img src="img/arp2.webp" alt=""></a>
+                </div>
+                <div class="imagem-produtos">
+                <a href="detalhes.php?id=8"><img src="img/arpi3.webp" alt=""></a>
+                </div>
+                <div class="imagem-produtos">
+                <a href="detalhes.php?id=9"><img src="img/arpi4.jpg" alt=""></a>
+                </div>
+                <div class="imagem-produtos">
+                <a href="detalhes.php?id=10"><img src="img/arpi5.webp" alt=""></a>
+                </div>
+            </div>
+
+            <div class="imagens-linha-produtos w100" ng-show="multisplit">
+                <div class="imagem-produtos">
+                <a href="detalhes.php?id=11"><img src="img/mul1.webp" alt=""></a>
+                </div>
+                <div class="imagem-produtos">
+                <a href="detalhes.php?id=12"><img src="img/mul2.webp" alt=""></a>
+                </div>
+                <div class="imagem-produtos">
+                <a href="detalhes.php?id=13"><img src="img/mul3.webp" alt=""></a>
+                </div>
+                <div class="imagem-produtos">
+                <a href="detalhes.php?id=14"><img src="img/mul4.png" alt=""></a>
+                </div>
+                <div class="imagem-produtos">
+                <a href="detalhes.php?id=15"><img src="img/mul5.png" alt=""></a>
+
+                </div>
+            </div>
+
+            <div class="imagens-linha-produtos w100" ng-show="energiasolar">
+                <div class="imagem-produtos">
+                <a href="detalhes.php?id=16"><img src="img/pl1.webp" alt=""></a>  
+                </div>
+                <div class="imagem-produtos">
+                <a href="detalhes.php?id=17"> <img src="img/pl2.jpg" alt=""></a> 
+                </div>
+                <div class="imagem-produtos">
+                <a href="detalhes.php?id=18">  <img src="img/pl3.jpg" alt=""></a>
+                </div>
+                <div class="imagem-produtos">
+                <a href="detalhes.php?id=19"> <img src="img/pl4.jpg" alt=""></a>
+                </div>
+                <div class="imagem-produtos">
+                <a href="detalhes.php?id=20"> <img src="img/pl5.png" alt=""></a>
                 </div>
             </div>
         </div>
-    <div>
-        <div class="linha w50">
-            <div ng-mouseover="funcaoArcondicionado()">
-               Ar-condicionado
-            </div>
-            <div ng-mouseover="funcaoInverter()">
-                Ar-condicionado Inverter
-            </div>
-            <div ng-mouseover="funcaoMultisplit()">
-                Multi Split
-            </div>
-            <div ng-mouseover="funcaoEnergiasolar()">
-                Energia solar
-            </div>
-        </div>
-
-        <div class="linha w100" ng-show="arcondicionado">
-            <div class="ajuste_img">
-             <a href="detalhes.php?id=1"><img src="img/arcondicionado1.webp" alt=""></a>
-            </div>
-            <div class="ajuste_img">
-             <a href="detalhes.php?id=2"><img src="img/arcondicionado2.webp" alt=""></a>
-            </div>
-            <div class="ajuste_img">
-             <a href="detalhes.php?id=3"><img src="img/arcondicionado3.webp" alt=""></a>
-            </div>
-            <div class="ajuste_img"> 
-             <a href="detalhes.php?id=4"><img src="img/arcondicionado4.webp" alt=""></a>
-            </div>
-            <div class="ajuste_img">
-             <a href="detalhes.php?id=5"><img src="img/arcondicionado5.webp" alt=""></a>
-            </div>
-        </div>
-
-        <div class="linha w100" ng-show="inverter">
-            <div class="ajuste_img">
-             <a href="detalhes.php?id=6"><img src="img/arp1.webp" alt=""></a>
-            </div>
-            <div class="ajuste_img">
-             <a href="detalhes.php?id=7"><img src="img/arp2.webp" alt=""></a>
-            </div>
-            <div class="ajuste_img">
-             <a href="detalhes.php?id=8"><img src="img/arpi3.webp" alt=""></a>
-            </div>
-            <div class="ajuste_img">
-             <a href="detalhes.php?id=9"><img src="img/arpi4.jpg" alt=""></a>
-            </div>
-            <div class="ajuste_img">
-             <a href="detalhes.php?id=10"><img src="img/arpi5.webp" alt=""></a>
-            </div>
-        </div>
-
-        <div class="linha w100" ng-show="multisplit">
-            <div class="ajuste_img">
-            <a href="detalhes.php?id=11"><img src="img/mul1.webp" alt=""></a>
-            </div>
-            <div class="ajuste_img">
-             <a href="detalhes.php?id=12"><img src="img/mul2.webp" alt=""></a>
-            </div>
-            <div class="ajuste_img">
-             <a href="detalhes.php?id=13"><img src="img/mul3.webp" alt=""></a>
-            </div>
-            <div class="ajuste_img">
-            <a href="detalhes.php?id=14"><img src="img/mul4.png" alt=""></a>
-            </div>
-            <div class="ajuste_img">
-            <a href="detalhes.php?id=15"><img src="img/mul5.png" alt=""></a>
-
-            </div>
-        </div>
-
-        <div class="linha w100" ng-show="energiasolar">
-            <div class="ajuste_img">
-              <a href="detalhes.php?id=16"><img src="img/pl1.webp" alt=""></a>  
-            </div>
-            <div class="ajuste_img">
-              <a href="detalhes.php?id=17"> <img src="img/pl2.jpg" alt=""></a> 
-            </div>
-            <div class="ajuste_img">
-              <a href="detalhes.php?id=18">  <img src="img/pl3.jpg" alt=""></a>
-            </div>
-            <div class="ajuste_img">
-               <a href="detalhes.php?id=19"> <img src="img/pl4.jpg" alt=""></a>
-            </div>
-            <div class="ajuste_img">
-               <a href="detalhes.php?id=20"> <img src="img/pl5.png" alt=""></a>
-            </div>
-        </div>
-    </div>
-</nav> -->
-
-<nav class="navbar-produtos">
-    <div class="cabecalho-produtos">
-        <h1>Produtos</h1>
-        <img src="img/linhapequena.png" alt="">
-    </div>
-    <div class="container-produtos">
-    <div class="img-produto">
-            kkkk
-        </div>
-        <div class="img-produto">
-            kkkk
-        </div>
-        <div class="img-produto">
-            kkkk
-        </div>
-        <div class="img-produto">
-            kkkk
-        </div>
-        <div class="img-produto">
-            kkkk
-        </div>
-    </div>
 </nav>
 </body>
 <script>
@@ -178,5 +143,6 @@ include 'conn/conecta.php';
             $scope.multisplit = false;
         }
     });
+</script>
 </script>
 </html>
